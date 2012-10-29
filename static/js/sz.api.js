607891cec93d5d9a431d8fa1da54a12952d94d15
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2012-10-30 00:59:50 +1000

    Запрос ресурса по абсолютному URI
    Запрос ресурса возможен с указанием как относительного, так и
    абсолютного URI. Если параметром метода выполнения запроса указывается
    строка, начинающаяся с 'http://' или 'https://', то она принимается за
    абсолютный URI. В противном случае - как URI относительно адреса API,
    указанного при создании sz.Api.
    
    Добавлены методы для запросов PUT и DELETE: sz.Api.put(resource,
    parameters, success) и sz.Api.del(resource, parameters, success)
    соответвенно.

--- static/js/sz.api.js
+++ static/js/sz.api.js
@@ -4,15 +4,20 @@ sz.Api = function(options){
     this.format = options.format;
     this.request_func = options.request_func
     this.request = function(verb, resource, parameters, success){
-        this.request_func({
-            url: this.uri + resource,
-            contentType: this.format = options.format,
-            type: verb,
-            data: parameters,
-            success: success
+        var resource_uri = resource;
+        if (resource_uri.indexOf('http://') == -1 && resource_uri.indexOf('https://') == -1)
+            resource_uri = this.uri + resource_uri;
+            this.request_func({
+                url: resource_uri,
+                contentType: this.format = options.format,
+                type: verb,
+                data: parameters,
+                success: success
             }
         );
     };
     this.get = function(resource, parameters, success) { return this.request('GET', resource, parameters, success); };
     this.post = function(resource, parameters, success) { return this.request('POST', resource, parameters, success); };
+    this.put = function(resource, parameters, success) { return this.request('PUT', resource, parameters, success); };
+    this.del = function(resource, parameters, success) { return this.request('DELETE', resource, parameters, success); };
 }
