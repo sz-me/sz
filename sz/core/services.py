b4edcf42ff975e920d29b03c461d4fe119be5880
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2012-11-11 22:40:52 +1000

    Рефакторинг

--- sz/core/services.py
+++ sz/core/services.py
@@ -3,13 +3,13 @@ from sz.core import utilities
 from sz.core.algorithms import lists
 
 class CategorizationService:
-    def detect_thinks(self, thinks, message):
+    def detect_thinks(self, things, message):
         words = set(utilities.words(message.text))
         detected_things = filter(
             lambda think: lists.any(
                 lambda word: word.startswith(think.stem),
                 words),
-            thinks
+            things
         )
         message.things = detected_things;
         return detected_things
