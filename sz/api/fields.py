ec9ed8b5b8610bb92c91682c6b9ea09d9ab6a814
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2012-12-05 20:49:37 +1000

    Поддержка PaginationSerializer в NestedField

--- sz/api/fields.py
+++ sz/api/fields.py
@@ -1,8 +1,11 @@
 from rest_framework import fields
+from sz.api import pagination
+from sz.api import services
 
 class NestedField(fields.Field):
     def __init__(self, *args, **kwargs):
         self.serializer = kwargs.pop('serializer', None)
+        self.paginate_by = kwargs.pop('paginate_by', None)
         assert self.serializer, 'serializer is required'
         super(NestedField, self).__init__(*args, **kwargs)
 
@@ -12,6 +15,11 @@ class NestedField(fields.Field):
         """
         if fields.is_simple_callable(value):
             value = value()
+        if issubclass(self.serializer, pagination.PaginationSerializer):
+            if self.paginate_by:
+                value = services.paginated_content(value, paginate_by=self.paginate_by)
+            else:
+                value = services.paginated_content(value)
         serializer = self.serializer(instance=value)
         return serializer.data
 
