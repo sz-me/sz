4910347167c3de097759f8edbfb39721d107dcfc
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2012-12-06 23:11:42 +1000

    Новые функции для работы со списками
    * all - возвращает истину только в том случае, если каждый элемент
    списка удовлетворяет заданному условию
    * first - возвращает первый элемент списка
    * last - возвращает последний элемент списка

--- sz/core/algorithms/lists.py
+++ sz/core/algorithms/lists.py
@@ -3,3 +3,9 @@ def debug_info(field, list, msg=''):
     return functools.reduce(lambda acc, item: acc + '\n ' + field(item), list, msg)
 def any(condition, list):
     return functools.reduce(lambda acc, item: acc or item, map(condition, list), 0)
+def all(condition, list):
+    return functools.reduce(lambda acc, item: acc and item, map(condition, list), 1)
+def first(list):
+    return functools.reduce(lambda first, second: first, list)
+def last(list):
+    return functools.reduce(lambda first, second: second, list)
--- sz/core/tests.py
+++ sz/core/tests.py
@@ -15,6 +15,15 @@ class ListsTest(TestCase):
         self.assertFalse(lists.any(lambda x: x < 0, [1, 2, 3]))
         self.assertFalse(lists.any(lambda x: x != None, [None, None, None]))
         self.assertTrue(lists.any(lambda x: x > 1, [0, 1, 2]))
+    def test_all(self):
+        self.assertTrue(lists.all(lambda x: x > 0, [1, 2, 3]))
+        self.assertFalse(lists.all(lambda x: x == None, set([None, 1])))
+        self.assertFalse(lists.all(lambda x: x > 1, [0, 1, 2]))
+    def test_first(self):
+        self.assertEquals(lists.first([4, 2, 3]), 4)
+        self.assertEquals(lists.first(set([4, 5, 3, 'a', 18])), 'a')
+    def test_last(self):
+        self.assertEquals(lists.last([4, 2, 3]), 3)
 """
 class TagsServicesTest(TestCase):
     def setUp(self):
