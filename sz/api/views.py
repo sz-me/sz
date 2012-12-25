5dbf6067f204b3afcd4c632ae12342d2933a644c
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2012-12-26 03:24:36 +1000

    Убраны лишние эндпоинты

--- sz/api/views.py
+++ sz/api/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime, exceptions
+import datetime
 from django.contrib.auth.models import User
 from django.http import Http404
 from rest_framework import permissions, status
@@ -30,39 +30,10 @@ class SzApiView(APIView):
 class ApiRoot(SzApiView):
     def get(self, request, format=None):
         return Response({
-            'messages': reverse('message-list'),
             'cities': reverse('city-list'),
             'places': reverse('place-list'),
             'users': reverse('user-list'),
-            #'things': reverse('thing-list'),
-            'categories': reverse('category-list'),
         })
-
-class MessageRoot(SzApiView):
-    """
-    List all messages, or create a new message.
-    """
-    def get(self, request, format=None):
-        queryset = models.Message.objects.order_by('date').all()
-        list_serializer = serializers.PaginatedMessageSerializer
-        list = self._get_list(queryset, request, list_serializer)
-        return Response(list)
-
-    def post(self, request, format=None):
-        print request.DATA
-        serializer = serializers.MessageSerializer(data=request.DATA)
-        if serializer.is_valid():
-            message = serializer.object
-            print message
-            message.user = request.user
-            print message.user
-            message.save()
-            print "message.save() OK"
-            things = models.Thing.objects.all()
-            categorization_service = services.CategorizationService()
-            categorization_service.detect_things(things, message)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class MessageInstance(SzApiView):
     """
@@ -192,51 +163,17 @@ class PlaceInstance(SzApiView):
 
     def get(self, request, pk, format=None):
         place = self.get_object(pk)
-        serializer = serializers.PlaceSerializer(instance=place)
+        #print u', '.join([u'#%s: %s' % (result['pk'], result['num_messages']) for result in queries.categories(place)])
+        position = {
+            'latitude': request.QUERY_PARAMS['latitude'],
+            'longitude': request.QUERY_PARAMS['longitude'],
+            'accuracy': request.QUERY_PARAMS.get('accuracy'),
+            }
+        serializer = serializers.PlaceSerializer(instance=place,
+            latitude=position['latitude'], longitude=position['longitude'],
+        )
         return Response(serializer.data)
 
 class PlaceMessages(SzApiView):
     def post(self, request, pk, format=None):
         pass
-
-class ThingRoot(SzApiView):
-    """
-    List all messages, or create a new message.
-    """
-    def get(self, request, format=None):
-        things = models.Thing.objects.all()
-        serializer = serializers.ThingSerializer(instance=things)
-        return Response(serializer.data)
-
-class ThingInstance(SzApiView):
-    def get_object(self, pk):
-        try:
-            return models.Thing.objects.get(pk=pk)
-        except models.Thing.DoesNotExist:
-            raise Http404
-    def get(self, request, pk, format=None):
-        thing = self.get_object(pk)
-        serializer = serializers.ThingSerializer(instance=thing)
-        return Response(serializer.data)
-
-class ThingMessages(SzApiView):
-    def get_object(self, pk):
-        try:
-            return models.Thing.objects.get(pk=pk)
-        except models.Thing.DoesNotExist:
-            raise Http404
-    def get(self, request, pk, format=None):
-        messages = self.get_object(pk).message_set.all()
-        serializer = serializers.MessageSerializer(instance=messages)
-        return Response(serializer.data)
-
-class CategoryRoot(SzApiView):
-    """
-    List all messages, or create a new message.
-    """
-    paginate_by = 4
-    def get(self, request, format=None):
-        queryset = models.Category.objects.all()
-        list_serializer = serializers.PaginatedCategorySerializer
-        list = self._get_list(queryset, request, list_serializer)
-        return Response(list)
