76d0a60678af091a22a6cda384bc4fa889e213d7
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2012-12-31 15:57:25 +1000

    Функция "безопасного" извлечения данных

--- sz/core/utils.py
+++ sz/core/utils.py
@@ -3,3 +3,9 @@ def safe_cast(val, to_type, default=None):
         return to_type(val)
     except Exception:
         return default
+
+def safe_get(val, func, default=None):
+    try:
+        return func(val)
+    except Exception:
+        return default
