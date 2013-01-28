f3f0b94c7c4d4c00ed27f831af27f6a3029b94f1
beebikun <beebikun@gmail.com>
2013-01-28 10:48:36 +1000

    Добавлена листалка сообщений(Пока листает во всех местах сразу)

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,9 +1,10 @@
 'use strict';
-angular.module('sz.client', ['sz.client.directives', 'sz.client.services'])
+angular.module('sz.client', ['sz.client.directives', 'sz.client.services','ui'])
 	.config(['$routeProvider', function($routeProvider) {$routeProvider
 		.when('/q/:query/:location', {templateUrl: 'partials/feed.html', controller: FeedController})
 		.when('/q/:query', {templateUrl: 'partials/feed.html', controller: FeedController})
 		.when('/v/:placeId', {templateUrl: 'partials/place.html', controller: PlaceController})
 		.otherwise({redirectTo: '/q//city'});
+        
 }])
 .constant('ENDPOINT','LOCAL');
