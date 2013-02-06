a061b1fc9499591b24473ff42b81bd751dd05d65
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2013-02-07 00:10:35 +1000

    Админка под новую модель категоризации

--- sz/api/admin.py
+++ sz/api/admin.py
@@ -6,44 +6,34 @@ class CategoryAdmin(admin.ModelAdmin):
     def things_to_str(self, obj):
         return ', '.join(map(lambda x: u'%s' % x, obj.thing_set.all()))
     things_to_str.short_description = u'Вещи'
-    list_display = ('name', 'body_part', 'gender', 'layer', 'things_to_str',)
-    list_filter = ('body_part', 'gender', 'layer',)
+    list_display = ('alias','name', 'keywords',)
     list_per_page = 50
     ordering = ['name']
 
 admin.site.register(models.Category, CategoryAdmin)
 
-class ThingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'stem', 'category',)
-    list_filter = ('category',)
-    list_per_page = 50
-    ordering = ['name']
-
-admin.site.register(models.Thing, ThingAdmin)
 
 from sz.core import services
+'''
 def detect_things(modeladmin, request, queryset):
     things = models.Thing.objects.all()
     categorization = services.CategorizationService(things)
     [categorization.detect_things(message) for message in queryset]
 detect_things.short_description = u"Развесить ярлычки"
+'''
 
 class MessageAdmin(admin.ModelAdmin):
-    def things_to_str(self, obj):
-        return ', '.join(map(lambda x: u'%s' % x, obj.things.all()))
-    things_to_str.short_description = u'Вещи'
     def categories_to_str(self, obj):
-        tags = set(['%s' % thing.category for thing in obj.things.all()])
-        return ', '.join(tags)
+        return ', '.join(map(lambda x: u'%s' % x, obj.categories.all()))
     categories_to_str.short_description = u'Категории'
     admin_thumbnail = AdminThumbnail(image_field='thumbnail')
     list_display = ('date', 'user', 'text', 'admin_thumbnail', 'place',
-                    'things_to_str', 'categories_to_str')
+                    'categories_to_str')
     list_display_links = ('text',)
     list_per_page = 25
     ordering = ['-date']
-    actions = [detect_things]
-    readonly_fields= ['things']
+    #actions = [detect_things]
+    #readonly_fields= ['things']
 
 admin.site.register(models.Message, MessageAdmin)
 '''
