2e1dfd3c94c9b6d88aa3a30f7e237c0c3f013297
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2013-02-15 16:21:27 +1000

    fix: ошибки формирования запроса мест для ленты новостей

--- sz/core/queries.py
+++ sz/core/queries.py
@@ -13,11 +13,6 @@ DEFAULT_DISTANCE = settings.DEFAULT_DISTANCE
 # TODO: вынести в sz.settings
 DEFAULT_PAGINATE_BY = 7
 
-def stems_stem_Q(stems):
-    return functools.reduce(lambda f, s: f | Q(stems__stem__icontains=s[0]), stems, Q())
-
-def place_name_Q(stems):
-    return functools.reduce(lambda f, s: f | Q(name__icontains=s[0]), stems, Q())
 
 def feed(**kwargs):
     """
@@ -30,12 +25,12 @@ def feed(**kwargs):
     assert latitude and longitude, 'latitude and longitude are required'
     current_position = fromstr("POINT(%s %s)" % (longitude, latitude))
     paginate_by = kwargs.get('paginate_by', DEFAULT_PAGINATE_BY)
-    stems = kwargs.get('stems', None)
+    stems = kwargs.get('stems', [])
     nearby = kwargs.get('nearby', None)
     category = kwargs.get('category', None)
 
-    filtered_places = models.Place.objects\
-        .filter(message__stems__in=models.Message.objects.filter(stems_stem_Q(stems)))
+    filtered_places = models.Place.objects.annotate(last_message=dj_models.Max('message__id'))\
+        .filter(last_message__isnull=False)
     if nearby is None:
         # TODO: определять город по координатам
         city_id = kwargs.pop('city_id', None)
@@ -44,26 +39,23 @@ def feed(**kwargs):
     else:
         distance = utils.safe_cast(nearby, int, DEFAULT_DISTANCE)
         distance_kwargs = {'m':'%i' % distance}
-        filtered_places = filtered_places.filter(position__distance_lte=(current_position, D(**distance_kwargs) ))\
+        filtered_places = filtered_places.filter(position__distance_lte=(current_position, D(**distance_kwargs)))\
             .distance(current_position).order_by('distance')
+    if len(stems) > 0:
+        filtered_places = filtered_places.filter(message__stems__stem__in=[stem[0] for stem in stems])
     if category is not None:
         filtered_places = filtered_places.filter(message__categories__in=[category,])
-    '''
-    if stems:
-        if len(stems) > 0:
-            filtered_places = filtered_places.filter(place_name_Q(stems))
-    '''
-    query = filtered_places.annotate(last_message=dj_models.Max('message__id'))\
-            .order_by('-last_message')[:paginate_by]
+    query = filtered_places.order_by('-last_message')[:paginate_by]
     return query
 
 
 def messages(places, **kwargs):
     paginate_by = kwargs.get('paginate_by', DEFAULT_PAGINATE_BY)
-    stems = kwargs.get('stems', None)
+    stems = kwargs.get('stems', [])
     category = kwargs.get('category', None)
-    filtered_messages = models.Message.objects.filter(place__pk__in=[p.pk for p in places])\
-        .filter(stems__stem__in=[stem[0] for stem in stems])
+    filtered_messages = models.Message.objects.filter(place__pk__in=[p.pk for p in places])
+    if len(stems) > 0:
+        filtered_messages = filtered_messages.filter(stems__stem__in=[stem[0] for stem in stems])
     if category is not None:
         filtered_messages = filtered_messages.filter(categories__in=[category,])
     query = filtered_messages.order_by('-date')[:paginate_by]
@@ -75,7 +67,7 @@ def categories(place):
     query = models.Category.objects\
     .filter(thing__message__place_id=place.id, thing__message__date__gte=last_day)\
     .values('name').annotate(
-        count = dj_models.Count('thing__message'),
-        last = dj_models.Max('thing__message__date'))\
+        count=dj_models.Count('thing__message'),
+        last=dj_models.Max('thing__message__date'))\
     .order_by('-count', '-last')
     return query
