d0d277fe538ee6ba73025eba25e63ed1e1318196
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2013-02-19 01:27:09 +1000

    Поправлены роуты в клиенте

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,11 +1,10 @@
 'use strict';
 angular.module('sz.client', ['sz.client.directives', 'sz.client.services','ui'])
 	.config(['$routeProvider', function($routeProvider) {$routeProvider
-		.when('/q/:query/:location', {templateUrl: 'partials/feed.html', controller: FeedController})
-		.when('/q/:query', {templateUrl: 'partials/feed.html', controller: FeedController})
-                .when('/place_list:query', {templateUrl: 'partials/place_list.html', controller: PlaceListController})
+		.when('/place/feed', {templateUrl: 'partials/feed.html', controller: FeedController})
+                .when('/place/search', {templateUrl: 'partials/place_list.html', controller: PlaceListController})
 		.when('/v/:placeId', {templateUrl: 'partials/place.html', controller: PlaceController})
-		.otherwise({redirectTo: '/q//city'});
+		.otherwise({redirectTo: '/place/feed'});
         
 }])
 .constant('ENDPOINT','LOCAL');
