e769140703a80af5b3bb7f6537ec330f2921d0db
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2013-02-24 15:56:05 +1000

    Изменён роутинг place/feed -> newsfeed

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,12 +1,10 @@
 'use strict';
 angular.module('sz.client', ['sz.client.directives', 'sz.client.services'])
 	.config(['$routeProvider', function($routeProvider) {$routeProvider
-		.when('/q/:query/:location', {templateUrl: 'partials/feed.html', controller: FeedController})
-		.when('/q/:query', {templateUrl: 'partials/feed.html', controller: FeedController})
-                .when('/new_message:query', {templateUrl: 'partials/new_message.html', controller: NewMessageController})
-		.when('/place/feed', {templateUrl: 'partials/feed.html', controller: FeedController})
+        .when('/newsfeed', {templateUrl: 'partials/feed.html', controller: FeedController})
+        .when('/new_message:query', {templateUrl: 'partials/new_message.html', controller: NewMessageController})
 		.when('/v/:placeId', {templateUrl: 'partials/place.html', controller: PlaceController})
-		.otherwise({redirectTo: '/place/feed'});
+		.otherwise({redirectTo: '/newsfeed'});
         
 }])
 .constant('ENDPOINT','LOCAL');
