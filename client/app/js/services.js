516b8a82f31ad96f5d5eaa7a3c9b8628b0d98e51
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2013-02-28 01:38:12 +1000

    Изменён сервис геолокации

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -10,8 +10,37 @@ szClient.factory('feedService', function($resource){
     });
 });
 
+szClient.factory('geolocationService', function ($rootScope) {
+    return {
+        getCurrentPosition: function (onSuccess, onError, options) {
+            navigator.geolocation.getCurrentPosition(function () {
+                    var that = this,
+                        args = arguments;
+
+                    if (onSuccess) {
+                        $rootScope.$apply(function () {
+                            onSuccess.apply(that, args);
+                        });
+                    }
+                }, function () {
+                    var that = this,
+                        args = arguments;
+
+                    if (onError) {
+                        $rootScope.$apply(function () {
+                            onError.apply(that, args);
+                        });
+                    }
+                },
+                options);
+        }
+    };
+});
+
+/*
 szClient.factory('geolocationService', ['$q', '$rootScope', function($q, $rootScope) {
     return function() {
+
         var changeLocation= function (coords) {
             $rootScope.$broadcast("locationChanged", {
                 coordinates: coords
@@ -23,9 +52,10 @@ szClient.factory('geolocationService', ['$q', '$rootScope', function($q, $rootSc
                 if (navigator.geolocation) {
                     navigator.geolocation.getCurrentPosition(
                         function (position) {
-                            $rootScope.$apply(function () {
-                                changeLocation(position.coords);
-                            });
+                            d.resolve(position)
+                            //$rootScope.$apply(function () {
+                            //    changeLocation(position.coords);
+                            //});
                         },
                         function (error) {
                             d.reject(error);
@@ -33,7 +63,7 @@ szClient.factory('geolocationService', ['$q', '$rootScope', function($q, $rootSc
                     );
                 }
                 else {
-                    d.reject('location services not allowed');
+                    d.reject('Geolocation service is not available!');
                 }
             }
             catch (err) {
@@ -41,8 +71,35 @@ szClient.factory('geolocationService', ['$q', '$rootScope', function($q, $rootSc
             }
         }, 1000);
         return d.promise;
-    };}]);
 
+        var deferred = $q.defer();
+
+        setTimeout(function() {
+            // since this fn executes async in a future turn of the event loop, we need to wrap
+            // our code into an $apply call so that the model changes are properly observed.
+            $rootScope.$apply(function() {
+                if (navigator.geolocation) {
+
+                    navigator.geolocation.getCurrentPosition(
+
+                        function (position) { deferred.resolve(position); },
+                        function (error){ deferred.reject(error); },
+                        {
+                            enableHighAccuracy: false,
+                            timeout: 30000,
+                            maximumAge: 0
+                        }
+                    );
+
+                } else {
+                    deferred.reject({message:'Geolocation service is not available!'});
+                }
+            });
+        }, 1000);
+
+        return deferred.promise;
+    };}]);
+*/
 szClient.factory('shmotCategoryService', function($resource){
                 return $resource('../../api/categories', {}, {
                         query: { method:'GET' }
