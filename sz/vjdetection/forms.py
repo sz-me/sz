d077c12c46d2f02d88baacfe3c69a7c341e234a4
Dmitry Kolyagin <zayacdrugmedvezhonka@gmail.com>
2013-05-07 11:02:17 +1000

    Удалено приложение vjdetection

--- sz/vjdetection/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from sz.vjdetection.detectors import CascadeFileManager
-
-
-class UploadPhotoForm(forms.Form):
-    photo = forms.ImageField()
-    haar_cascade = forms.ChoiceField(choices=CascadeFileManager.choices())
-    #scaleFactor = forms.FloatField
